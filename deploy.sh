1c50b29343ac36ac0ed7b31466ffe437414c522e
Vadim Venediktov <install.vv@gmail.com>
2013-01-24 16:41:06 +0400

    Ench: [Config] Added migrate task to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ssh moxopal@installero.com 'cd www && git pull && bundle install && touch tmp/restart.txt'
+ssh moxopal@installero.com 'cd www && git pull && bundle install && bundle exec rake db:migrate&& touch tmp/restart.txt'
