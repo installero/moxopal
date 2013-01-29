3829033fa7895eb9fcbc5c737b67ee4c77842d1d
Vadim Venediktov <install.vv@gmail.com>
2013-01-29 20:35:58 +0400

    Fix: deploy.sh fixed

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ssh moxopal@installero.com 'cd www && git pull && bundle install && bundle exec rake db:migrate RAILS_ENV=production&& touch tmp/restart.txt'
+ssh moxopal@installero.com 'cd www && git pull && bundle install && bundle exec rake db:migrate RAILS_ENV=production && touch tmp/restart.txt'
